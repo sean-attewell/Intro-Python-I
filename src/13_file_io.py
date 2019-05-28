227ba83114f816f6eb0933852864c0398b80d219
sean-attewell <sean.attewell@gmail.com>
2019-05-28 15:57:55 +0100

    14 of 15 exercises done

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -8,11 +8,22 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
-# YOUR CODE HERE
+with open('foo.txt', 'r') as f:
+    read_data = f.read()
+    print(read_data)
+# If you’re not using the with keyword, then you should call f.close()
+# to close the file and immediately free up any system resources used by it.
+
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
-# then close the file. Open up "bar.txt" and inspect it to make 
+# then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+with open("bar.txt", "w") as f:
+    f.write("The pale has leaks, and even if\n")
+    f.write("you put all your water into it,\n")
+    f.write("You'll end up with nothing left to drink.")
+newdoc = open("bar.txt")
+print(newdoc.read())
+newdoc.close()
